5a5be3da95daabcd8373416b842fc1a57ce19886
Henry <HDERR001@plattsburgh.edu>
2022-02-21 20:20:31 -0500

    detail view commit

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,18 +3,27 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Parcel
 public class Movie {
+    int movieID;
     String posterPath;
     String title;
     String overview;
+    double rating;
+    // empty constructor required by the parceler library
+    public Movie() {}
+
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieID = jsonObject.getInt("id");
+
     }
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException{
         List<Movie> movies = new ArrayList<>();
@@ -22,6 +31,10 @@ public class Movie {
             movies.add(new Movie(movieJsonArray.getJSONObject(i)));
         }
         return movies;
+    }
+
+    public int getMovieID() {
+        return movieID;
     }
 
     public String getTitle() {
@@ -34,5 +47,9 @@ public class Movie {
 
     public String getPosterPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public double getRating() {
+        return rating;
     }
 }
